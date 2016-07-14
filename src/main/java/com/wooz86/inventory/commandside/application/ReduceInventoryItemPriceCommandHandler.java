f7e5e41c98748a76118b5c77d34c27e94496df0c
Joel Thoresson <joel@netconsult.se>
2016-07-14 23:21:53 +0200

    Imported class instead of using a fully qualified class name

--- src/main/java/com/wooz86/inventory/commandside/application/ReduceInventoryItemPriceCommandHandler.java
+++ src/main/java/com/wooz86/inventory/commandside/application/ReduceInventoryItemPriceCommandHandler.java
@@ -1,5 +1,6 @@
 package com.wooz86.inventory.commandside.application;
 
+import com.wooz86.api.ReduceInventoryItemPriceCommand;
 import com.wooz86.framework.application.CommandHandler;
 import com.wooz86.framework.domain.Repository;
 import com.wooz86.framework.eventstore.AggregateNotFoundException;
@@ -8,7 +9,7 @@ import com.wooz86.inventory.commandside.domain.model.InventoryItem;
 
 import java.io.IOException;
 
-public class ReduceInventoryItemPriceCommandHandler implements CommandHandler<com.wooz86.api.ReduceInventoryItemPriceCommand> {
+public class ReduceInventoryItemPriceCommandHandler implements CommandHandler<ReduceInventoryItemPriceCommand> {
 
     private final Repository repository;
 
@@ -16,7 +17,7 @@ public class ReduceInventoryItemPriceCommandHandler implements CommandHandler<co
         this.repository = repository;
     }
 
-    public void handle(com.wooz86.api.ReduceInventoryItemPriceCommand command) throws InstantiationException, IllegalAccessException, IOException, ClassNotFoundException, ConcurrencyException, AggregateNotFoundException {
+    public void handle(ReduceInventoryItemPriceCommand command) throws InstantiationException, IllegalAccessException, IOException, ClassNotFoundException, ConcurrencyException, AggregateNotFoundException {
 
         // Get from repository
         InventoryItem inventoryItem = repository.getById(command.getAggregateId());
